8f4ac38cdbe18fdae6bdd603f3befa4dba600abc
connorkoch0511 <connor.koch@student.csulb.edu>
2026-02-03 23:19:18 -0800

    updates routes

--- infra/routes.tf
+++ infra/routes.tf
@@ -1,11 +1,11 @@
 resource "aws_apigatewayv2_route" "post_contact" {
   api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "POST /contact"
-  target    = "integrations/${aws_apigatewayv2_integration.projects.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.contact.id}"
 }
 
-resource "aws_apigatewayv2_route" "any_contact_api" {
+resource "aws_apigatewayv2_route" "get_projects" {
   api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "ANY /contact-api"
-  target    = "integrations/${aws_apigatewayv2_integration.contact.id}"
+  route_key = "GET /projects"
+  target    = "integrations/${aws_apigatewayv2_integration.projects.id}"
 }
