0668e57dd3e22cd95f51d2873baa1c85dccaae5b
connorkoch0511 <connor.koch@student.csulb.edu>
2026-02-03 22:28:28 -0800

    add back api outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -10,13 +10,13 @@ output "dynamodb_table_arn" {
   value = aws_dynamodb_table.contact_messages.arn
 }
 
-#output "http_api_id" {
-  #value = aws_apigatewayv2_api.http_api.id
-#}
+output "http_api_id" {
+  value = aws_apigatewayv2_api.http_api.id
+}
 
-#output "http_api_endpoint" {
-  #value = aws_apigatewayv2_api.http_api.api_endpoint
-#}
+output "http_api_endpoint" {
+  value = aws_apigatewayv2_api.http_api.api_endpoint
+}
 
 output "cloudfront_distribution_id" {
   value = var.cloudfront_distribution_id
