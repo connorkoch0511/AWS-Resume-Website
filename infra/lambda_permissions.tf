5adfb9fcc96826925697563b2ab2b42ff8fa8961
connorkoch0511 <connor.koch@student.csulb.edu>
2026-02-04 23:17:16 -0800

    update terraform permissions

--- infra/lambda_permissions.tf
+++ infra/lambda_permissions.tf
@@ -1,19 +1,19 @@
-# Allow API Gateway to invoke projects-api (GET /projects)
+data "aws_caller_identity" "current" {}
+
 resource "aws_lambda_permission" "apigw_projects_get" {
-  statement_id  = "AllowExecutionFromAPIGatewayGetProjects"
+  statement_id  = "apigw-3qo252mmdj-get-projects"
   action        = "lambda:InvokeFunction"
   function_name = "projects-api"
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "arn:aws:execute-api:${var.aws_region}:814835236587:${aws_apigatewayv2_api.http_api.id}/*/GET/projects"
+  source_arn = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${aws_apigatewayv2_api.http_api.id}/*/GET/projects"
 }
 
-# Allow API Gateway to invoke contact-api (POST /contact)
 resource "aws_lambda_permission" "apigw_contact_post" {
-  statement_id  = "AllowExecutionFromAPIGatewayPostContact"
+  statement_id  = "apigw-3qo252mmdj-post-contact"
   action        = "lambda:InvokeFunction"
   function_name = "contact-api"
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "arn:aws:execute-api:${var.aws_region}:814835236587:${aws_apigatewayv2_api.http_api.id}/*/POST/contact"
+  source_arn = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${aws_apigatewayv2_api.http_api.id}/*/POST/contact"
 }
